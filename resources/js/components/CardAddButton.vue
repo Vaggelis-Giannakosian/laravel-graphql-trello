ae118425602ab3ef90a928259a73d35589beafa1
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-12-29 16:54:40 +0200

    added CardAdd mutation to CardAddButton.vue component

--- resources/js/components/CardAddButton.vue
+++ resources/js/components/CardAddButton.vue
@@ -1,12 +1,30 @@
 <template>
-    <div class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm">
+    <div @click="addCard" class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm">
         Add new card
     </div>
 </template>
 
 <script>
+    import CardAdd from './../graphql/CardAdd.gql'
+
     export default {
-        name: "CardAddButton"
+        name: "CardAddButton",
+        props: ['listId,ownerId'],
+        methods:{
+            addCard(){
+
+                this.$apollo.mutate({
+                    mutation:CardAdd,
+                    variables: {
+                        'title':'Added through mutation',
+                        'order':3,
+                        'listId': 1,
+                        'ownerId':1,
+                    }
+                })
+
+            }
+        }
     }
 </script>
 
