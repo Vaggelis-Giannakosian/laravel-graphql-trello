0163d846e029530312221501efee1ce8e7475af2
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-12-31 15:42:01 +0200

    created login - logout mutations along with their respective Resolvers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin','*')
+            ->header('Access-Control-Allow-Origin','http://localhost:3000')
+            ->header('Access-Control-Allow-Credentials','true')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers','Origin, Content-Type, Authorization, X-REQUESTED-WITH, X-CSRF-TOKEN');
     }
