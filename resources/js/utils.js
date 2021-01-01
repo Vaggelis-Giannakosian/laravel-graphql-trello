71f69c9e44c78508db878f8da07ee3890eb09e8f
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2021-01-01 12:47:12 +0200

    added AuthenticateUser listener to authenticate user after registration

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -4,10 +4,25 @@ export function normalizeGQLErrors(err) {
     const hasInternal = errors => errors.some(e => e.internal);
     const replaceInternal = (errors, err) => hasInternal(errors) ? errors.filter(e => !e.internal).concat(err) : errors;
 
-    return replaceInternal((err?.graphQLErrors || []).map(error => ({
-        message: error.message,
-        internal: Boolean(!error?.path?.length)
-    })), {
+    return replaceInternal((err?.graphQLErrors || []).map(error => {
+
+        if("validation" === error.extensions?.category)
+        {
+            const validationErrors = error.extensions.validation || {}
+            return Object.keys( validationErrors )
+                .flatMap(key=> validationErrors[key])
+                .map(e=>({
+                    message: e.replace('input.',''),
+                    internal: false
+                }));
+        }
+
+        return {
+            message: error.message,
+            internal: Boolean(!error?.path?.length)
+        }
+
+    }), {
         message: 'You must provide your email and password'
-    })
+    }).flat()
 }
